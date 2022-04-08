f73b91b1f6d27f5ae7fb280158638445054c9405
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-04-08 14:46:00 -0500

    Se agrego archivo generador de password

---
+++ gen.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+# Generador de password simple
+import caracteres as car 
+import random
+import time
+
+def banner():
+    print("="*36)
+    print("[+] Generado password by jakepy [+]")
+    print("="*36)
+
+
+def leingth_pass():
+    n = int(input("\n[+] Ingrese la longitud de la password: "))
+    return n
+
+
+def generate(n):
+    all_caracteres = car.CHARS + car.MAYUS + car.MINUS + car.NUMS
+
+    passwords = []
+
+    for i in range(n):
+        caracteres_ramdom = random.choice(all_caracteres)
+        passwords.append(caracteres_ramdom)
+
+    passwords = "".join(passwords)
+    return passwords
+
+def main():
+    banner()
+    password = generate(leingth_pass())
+    print("\n[+] Generando...\n")
+    time.sleep(1.5)
+    print(f"[+] Su nueva password es: {password}")
+
+if __name__ == '__main__':
+    main()
