28ab202e8bf81c7b256db2ea2397df3678b62385
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-04-23 07:59:22 -0500

    se agregaron parmisos

--- gen.py
+++ gen.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
+# _*_ coding: utf-8 -*-
+#
+# Creador: Jakepy Perdomo <j4kyjak3@protonmail.com>
+# Título: Generadir de contraseña
+# FechaCreación: 17-abr-22
+#
+# Código simple de un generador de contraseña.
+# Copyright (c) 2022 Jakepy Perdomo <j4kyjak3@protonmail.com>. 
+# Comentarios bienvenidos.
+#
+# Permiso otorgado para uso y distribución no comercial siempre que
+# este aviso de derechos de autor permanece intacto.
+#
+# Hay ejemplos de cómo usar este código en el README.md.
+#
+# Cual quier duda escribeme, o talvez este dormido jsjs.
+# 
 
-# Generador de password simple
 import caracteres as car 
 import random
 import time
