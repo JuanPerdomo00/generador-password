90babee8a128439fb8700ad726896c505a88a2c9
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2023-10-20 22:35:42 -0500

    update

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+import setuptools
 
-setup(
-    name="Generador-Password",
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="passpyword",
     version="0.1.0",
-    description="Simple generador contrasenas",
+    script=["gen.py"],
+    description="fast password generator for keepass",
     author="Jakepys - JuanPerdomo00",
     author_email="j4kyjak3@protenmail.com",
+    long_description=description,
+    long_description_content_type="text/markdown",
     license="GPLv3",
     platforms="GNU/Linux",
     url="https://github.com/JuanPerdomo00/generador-password",
-    py_modules=["pyperclip3"]
+    packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+     ],
+     install_requires=["pyperclip3==0.4.1"],
 )
+
